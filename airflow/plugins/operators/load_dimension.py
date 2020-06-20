ecb3f18459ed8c87f6f0823bec85ee088a52b8af
lysmarfreitas <lysmar@gmail.com>
2020-06-20 15:06:20 -0300

    Update LoadDimensionOperator operator
    refactor: Update operator to add append-only and delete-load functionality
    
    Append-only and delete-load functionalityt was not covered in the LoadDimension operator.  To solve this I added a code
     , according to which it truncate the table followed by insert (overwrite) or just insert (append) according to the mode of flag loading required.

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -3,35 +3,38 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
-
     ui_color = '#80BD9E'
 
     insert_sql = """
-        TRUNCATE TABLE {};
         INSERT INTO {}
         {};
-        COMMIT;
     """
 
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="",
                  table="",
+                 delete_load=False,
                  load_sql_stmt="",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.table = table
+        self.delete_load = delete_load
         self.load_sql_stmt = load_sql_stmt
         
 
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        self.log.info(f"Loading data into {self.table} dimension table in Redshift...")
+        
+        if self.delete_load:
+            self.log.info("Clearing data from destination Redshift table")
+            redshift.run("Deleting from {}".format(self.table))
+
         formatted_sql = LoadDimensionOperator.insert_sql.format(
-            self.table,
             self.table,
             self.load_sql_stmt
         )
+        self.log.info(f"Executing {formatted_sql} ...")
         redshift.run(formatted_sql)
